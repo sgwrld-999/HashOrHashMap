21ccc2143bef9f5b33cdde893b6388a050b723e7
sgwrld-999 <130132265+sgwrld-999@users.noreply.github.com>
2023-10-24 15:35:46 +0530

    update

--- Medium/FindtheDuplicateNumber.cpp
+++ Medium/FindtheDuplicateNumber.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 class Solution {
 public:
+    //Method 1: Using sorting and then checking the adjacent elements
+    int sorting(vector<int>& nums){
+        sort(nums.begin(),nums.end());
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] == nums[i+1]) return nums[i];
+        }
+        return -1;
+    }
+    //Method 2: Using unordered_map to store the frequency of each element 
+    //If the frequency of any element is greater than 1 then return that element
     int findDuplicate(vector<int>& nums) {
         unordered_map<int,int>mp;
         for(int i = 0 ; i < nums.size() ; i++){
